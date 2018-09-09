ca775a148ac1e2d3d9acd26292664fe2f094c09c
dhadbs2 <43092216+dhadbs2@users.noreply.github.com>
2018-09-09 13:25:50 +0900

    수정

--- src/main/java/Stand.java
+++ src/main/java/Stand.java
@@ -4,7 +4,7 @@ public class Stand {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
-		System.out.println("자바 프로젝트");
+		System.out.println("자바 프로젝트!!!!!");
 	}
 
 }
