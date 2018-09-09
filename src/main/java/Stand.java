8b9686e7a7dda89ebe83f4147319fafe3877acb8
USER <USER@122.44.176.226>
2018-09-09 13:23:13 +0900

    인코딩 수정

--- src/main/java/Stand.java
+++ src/main/java/Stand.java
@@ -4,6 +4,7 @@ public class Stand {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		System.out.println("자바 프로젝트");
 	}
 
 }
